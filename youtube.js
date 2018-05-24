9f78c333830d1c07eee0a43ac00f459d800a8ba8
SatellaSoft <satellasoft@gmail.com>
2018-05-24 08:07:08 -0300

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -5,46 +5,65 @@
 //Web site: www.gunnarcorrea.com
 //Usage
 /*
-	var data = {
-		youtubeURL : GetValueId("youtubeURL"),
-		youtubeRESULT : "youtubeRESULT"
-		};
-	CreateYoutube(data);
-*/
+ var data = {
+ youtubeURL : GetValueId("youtubeURL"),
+ youtubeRESULT : "youtubeRESULT"
+ };
+ CreateYoutube(data);
+ */
 
-function CreateYoutube(data){
-	var queryName = "v";
-	var token = getParameterByName(queryName, data.youtubeURL);
-	var element = CreateYoutubeDiv(token);
-	SetElementId(element, data);
+function CreateYoutube(data) {
+    var r = Validate(data);
+    if (r != "OK") {
+        alert(r);
+        return;
+    }
+
+    var queryName = "v";
+    var token = getParameterByName(queryName, data.youtubeURL);
+    var element = CreateYoutubeDiv(token);
+    SetElementId(element, data);
 }
 
-var token = getParameterByName('v');
-			
-			
-function CreateYoutubeDiv(token){
-				var stringDiv = "<div class='videoWrapper'><iframe width='560' height='315'"+
-				"src='https://www.youtube.com/embed/"+token+"'"+ 
-				"frameborder='0' allow='autoplay; encrypted-media'"+
-				"allowfullscreen></iframe></div>";
-	return stringDiv;
+function Validate(data) {
+    if (data == null) {
+        return "Object date not informed";
+    } else if (data.youtubeRESULT.length == 0) {
+        return "Unspecified output element";
+    } else if (data.youtubeURL.length == 0) {
+        return "Video URL not specified";
+    } else {
+        return "OK";
+    }
 }
-			
+
+
+function CreateYoutubeDiv(token) {
+    var stringDiv = "<div class='videoWrapper'><iframe width='560' height='315'" +
+            "src='https://www.youtube.com/embed/" + token + "'" +
+            "frameborder='0' allow='autoplay; encrypted-media'" +
+            "allowfullscreen></iframe></div>";
+    return stringDiv;
+}
+
 function getParameterByName(name, url) {
-	//Get in Stackoverflow
-	if (!url) url = window.location.href;
-				name = name.replace(/[\[\]]/g, '\\$&');
-				var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-					results = regex.exec(url);
-				if (!results) return null;
-				if (!results[2]) return '';
-				return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-			
-function GetValueId(field){
-	return document.getElementById(field).value;
+    //Get in Stackoverflow
+    if (!url)
+        url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+    if (!results)
+        return null;
+    if (!results[2])
+        return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-function SetElementId(element, data){
-	document.getElementById(data.youtubeRESULT).innerHTML = element;
+function GetValueId(field) {
+    return document.getElementById(field).value;
+}
+
+function SetElementId(element, data) {
+    document.getElementById(data.youtubeRESULT).innerHTML = element;
 }
